ff6ef593b2c239f5744d056c142fe3b0e5fbee81
pefish <darrondarron@qq.com>
2024-11-13 10:18:56 +0800

    add

--- test/index.ts
+++ test/index.ts
@@ -3,12 +3,29 @@ import SequelizeHelper from "../src/mysql";
 
 Starter.startAsync(async () => {
   const sequelizeHelper = new SequelizeHelper({
-    host: "localhost",
-    username: "root",
-    password: "root",
-    database: "test",
+    host: "",
+    username: "pefish_me",
+    password: "",
+    database: "pefish_me",
   });
   await sequelizeHelper.init();
+
+  await sequelizeHelper.insert({
+    insert: {
+      address: "",
+      token_address: "111",
+      init_amount: "111",
+      init_token_amount: "111",
+      current_token_amount: "111",
+      init_timestamp: 1132231,
+      records: JSON.stringify([
+        {
+          aaa: "dvsdf",
+        },
+      ]),
+    },
+    from: "sol_pos",
+  });
 
   await sequelizeHelper.close();
   console.log(`11`);
